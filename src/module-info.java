ff676ab1475af8b7797c34ff61bda9bbfc55e34a
Vitor Brisola <vitoraugusto199@gmail.com>
2023-07-22 19:37:57 -0300

    first commit

--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,10 @@
 /**
  * 
  */
-module MyJavaGameEngine {
+module MyJavaGameEngine_2 {
+    requires org.lwjgl;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.opengl;
+    requires org.lwjgl.openal;
+    requires org.lwjgl.stb;
 }
